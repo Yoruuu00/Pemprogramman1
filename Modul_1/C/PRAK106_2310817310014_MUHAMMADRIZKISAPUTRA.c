97d0dcc826151cc30a17ce3d63a3c6b4086e1739
Yoruuu00 <144782663+Yoruuu00@users.noreply.github.com>
2023-10-02 20:06:23 +0800

    PRAK106_2310817310014_MUHAMMADRIZKISAPUTRA.c

--- Modul_1/C/PRAK106_2310817310014_MUHAMMADRIZKISAPUTRA.c
+++ Modul_1/C/PRAK106_2310817310014_MUHAMMADRIZKISAPUTRA.c
@@ -6,7 +6,7 @@ int a = 4;
 int b = 8;
 int c = 3;
    printf("variabel a bernilai %d\n",a);
-   printf("variuabel b bernilai %d\n", b);
+   printf("variabel b bernilai %d\n", b);
    printf("variabel c bernilai %d\n",c);
    printf("apakah a sama dengan b ? jawabnya adalah %d\n",a == b);
    printf("apakah b lebih besar dari c ? jawabnya adalah %d\n",b > c);
