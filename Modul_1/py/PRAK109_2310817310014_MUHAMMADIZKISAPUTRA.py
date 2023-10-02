c733395ea892fc3627aed2f70c2a5a899ed0cce6
Yoruuu00 <144782663+Yoruuu00@users.noreply.github.com>
2023-10-02 20:27:27 +0800

    PRAK109_2310817310014_MUHAMMADIZKISAPUTRA.py

--- Modul_1/py/PRAK109_2310817310014_MUHAMMADIZKISAPUTRA.py
+++ Modul_1/py/PRAK109_2310817310014_MUHAMMADIZKISAPUTRA.py
@@ -2,6 +2,6 @@ pasukan_yz= 958730
 pahlawan_altar = 5
 pasukan_yang_dikalahkan = int (pasukan_yz / pahlawan_altar)
 
-print("Jumlah pasukan yang dibawa yz =",pasukan_yz)
-print("Jumlah pahlawan =",pahlawan_altar)
+print("Jumlah pasukan yang dibawa yz = ?",pasukan_yz)
+print("Jumlah pahlawan = ?",pahlawan_altar)
 print("Jumlah pasukan yang harus dikalahkan setiap pahlawan adalah =", pasukan_yang_dikalahkan)
