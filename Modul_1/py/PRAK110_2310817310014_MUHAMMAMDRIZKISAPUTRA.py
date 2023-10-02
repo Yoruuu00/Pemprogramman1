70c4bfdaca88ff748bb4f7277a55c6a63e226210
Yoruuu00 <144782663+Yoruuu00@users.noreply.github.com>
2023-10-02 20:27:54 +0800

    PRAK110_2310817310014_MUHAMMAMDRIZKISAPUTRA.py

--- Modul_1/py/PRAK110_2310817310014_MUHAMMAMDRIZKISAPUTRA.py
+++ Modul_1/py/PRAK110_2310817310014_MUHAMMAMDRIZKISAPUTRA.py
@@ -6,7 +6,7 @@ luas_segitiga = (alas * tinggi) // 2
 
 print(f"Diketahui:\nAlas = {alas}\nTinggi = {tinggi}\n")
 print(f"Jawab:")
-print(f"Sisi A = {alas}")
-print(f"Sisi B = {tinggi}")
-print(f"Sisi C = {sisi_miring_segitiga}")
+print(f"Sisi A = ? {alas}")
+print(f"Sisi B = ? {tinggi}")
+print(f"Sisi C = ? {sisi_miring_segitiga}")
 print(f"Keliling = {kel_segitiga}\nLuas = {luas_segitiga}")
