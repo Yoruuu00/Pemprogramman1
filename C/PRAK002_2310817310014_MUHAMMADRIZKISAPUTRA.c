3ae39e5eb2372b6ae371bad368a63cb60714626f
Yoruuu00 <144782663+Yoruuu00@users.noreply.github.com>
2023-12-18 10:44:54 +0800

    Delete C directory

--- C/PRAK002_2310817310014_MUHAMMADRIZKISAPUTRA.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main(){
-    printf("Selamat Pagi, Muhammad Rizki Saputra\nSelamat Sore, Muhammad Rizki Saputra\nSelamat Malam, Muhammad Rizki Saputra");
-    return 0;
-}
--- C/PRAK003_2310817310014_MUHAMMADRIZKISAPUTRA.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Andi berkata \"Saya Pasti Bisa\"");
-    return 0;
-}
